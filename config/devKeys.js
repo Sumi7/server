a8c72183300263daf040f3585214941bf0ffde82
Sumit <sumitsati7@gmail.com>
2018-05-06 01:24:34 +0530

    pushing code for refernce

---
+++ config/devKeys.js
@@ -0,0 +1,11 @@
+module.exports = {
+    googleClientID: '654710432203-p3t2od1t05hnps7ked6nr1g36rn5lld0.apps.googleusercontent.com',
+    googleClientSecret: 'pg0PXqJkMm8k68qpoGTyaoi2',
+    githubClientID: '5829a2a7f90db9b6f4db',
+    githubClientSecret: '5b7682a799d863c537956eada2e77a927cc0056f',
+    mongoURI: 'mongodb://Sumit:MyPassword@ds163494.mlab.com:63494/feedback_app',
+    cookieKey: 'alsgduyavhsdmfasuydfja4s45dfsdfsf',
+    stripeSecretKey: 'sk_test_XF4kFLItm50cqwvlfuqRhOiS',
+    sendGridKey: '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',
+    redirectDomain: 'https://localhost:5000'
+}
---
+++ config/prodKeys.js
@@ -0,0 +1,11 @@
+module.exports = {
+    googleClientID: process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    githubClientID: process.env.GITHUB_CLIENT_ID,
+    githubClientSecret: process.env.GITHUB_CLIENT_SECRET,
+    mongoURI: process.env.MONGO_URI,
+    cookieKey: process.env.COOKIE_KEY,
+    stripeSecretKey: process.env.STRIPE_SK,
+    sendGridKey: process.env.SENDGRID_KEY,
+    redirectDomain: process.env.REDIRECT_DOMAIN
+}
